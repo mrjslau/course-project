f1772ecbd8a03a1fe7e672700c17dddc3ee2f6ca
Marijus Laucevicius <marijus.laucevicius@exanic.ch>
2021-07-05 17:42:18 +0200

    add shpping-list service

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,26 +1,22 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
-import { Ingredient } from 'src/app/shared/ingredient.model';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list-edit',
   templateUrl: './shopping-list-edit.component.html',
   styleUrls: ['./shopping-list-edit.component.css']
 })
-export class ShoppingListEditComponent implements OnInit {
+export class ShoppingListEditComponent{
   @ViewChild('nameInput') nameInputRef: ElementRef;
   @ViewChild('amountInput') amountInputRef: ElementRef;
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   onAdd() {
-    const ingredientName = this.nameInputRef.nativeElement.value;
-    const ingredientAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingredientName, ingredientAmount);
-    this.ingredientAdded.emit(newIngredient);
+    this.shoppingListService.addIngredient(
+      this.nameInputRef.nativeElement.value,
+      this.amountInputRef.nativeElement.value
+    );
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,12 @@
-export class ShoppingList {
-    
+import { Ingredient } from "../shared/ingredient.model";
+
+export class ShoppingListService {
+    ingredients: Ingredient[] = [
+        new Ingredient('lettuce', 1),
+        new Ingredient('dressing', 1)
+    ];
+
+    addIngredient(name: string, amount: number) {
+        this.ingredients.push(new Ingredient(name, amount));
+    }
 }
