a46172179d8387bd0c494fbb3b31a6febee3bdf8
mrjslau <marijuslau@yahoo.com>
2021-07-06 17:59:55 +0200

    add active links

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,20 +6,10 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  @Output() selectedSection = new EventEmitter<number>();
-
   collapsed = true;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  recipesSelected() {
-    this.selectedSection.emit(0);
-  }
-
-  shoppingListSelected() {
-    this.selectedSection.emit(1);
   }
 }
