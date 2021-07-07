f038daced68eef652760249ecab3438c5a5bb80d
mrjslau <marijuslau@yahoo.com>
2021-07-07 12:20:42 +0200

    add edit component

---
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+
+@Component({
+  selector: 'app-recipe-edit',
+  templateUrl: './recipe-edit.component.html',
+  styleUrls: ['./recipe-edit.component.css']
+})
+export class RecipeEditComponent implements OnInit {
+  recipe: Recipe;
+
+  constructor(private route: ActivatedRoute,
+    private recipeService: RecipeService) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(
+      (params: Params) => {
+        if (params['id']) {
+          this.recipe = this.recipeService.getRecipe(+params['id']);
+        } else {
+          this.recipe = new Recipe("Test", "Tasty", "", []);
+        }
+      }
+    );
+  }
+
+}
