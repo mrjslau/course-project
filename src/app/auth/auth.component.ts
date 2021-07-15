72e827e9a237974177b6fa8da488d19e1baf3ce8
mrjslau <marijuslau@yahoo.com>
2021-07-15 17:50:47 +0200

    add signup functionality, add loading spinner, improve error handling in signup

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,49 @@
 import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { AuthService } from "./auth.service";
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
 export class AuthComponent {
-  
+  isLoginMode = true;
+  isLoading = false;
+  error: string = null;
+
+  constructor(private authService: AuthService) {}
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+
+    const email = form.value['email'];
+    const password = form.value['password'];
+    
+    this.isLoading = true;
+
+    // SIGNIN
+    if (!this.isLoginMode) { 
+      this.authService.signup(email, password).subscribe(
+        data => {
+          console.log(data);
+          this.isLoading = false;
+        },
+        errorMessage => {
+          this.error = errorMessage;
+          this.isLoading = false;
+        }
+      )
+    // LOGIN
+    } else {
+
+    }
+
+    form.reset();
+  }
 }
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+interface AuthResponseData {
+  idToken: string,
+  email: string,
+  refreshToken: string,
+  expiresIn: string,
+  localId: string
+}
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+
+  constructor(private httpClient: HttpClient) {}
+
+  signup(email: string, password: string) {
+    return this.httpClient.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDCkeEyfgJkofP_8GX13LH4EJWY0BOojdc',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    )
+    .pipe(
+      catchError(errorRes => {
+        let errorMessage = 'An unknown error occurred!';
+        if(!errorRes.error || !errorRes.error.error) {
+          return throwError(errorMessage);
+        }
+        switch(errorRes.error.error.message) {
+          case 'EMAIL_EXISTS':
+            errorMessage = "This email exists";
+        }
+        return throwError(errorMessage);
+      })
+    )
+  }
+}
